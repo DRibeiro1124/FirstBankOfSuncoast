110d0e03dd7a7202bff0fa930faa59077a04e962
Dino Ribeiro <DRibeiro1124@gmail.com>
2023-01-04 19:00:28 -0500

    created Class Transaction

--- Program.cs
+++ Program.cs
@@ -2,11 +2,25 @@
 
 namespace FirstBankOfSuncoast
 {
-  class Program
-  {
-    static void Main(string[] args)
+
+    class Transaction
     {
-      Console.WriteLine("Welcome to C#");
+        public int Amount { get; set; }
+
+        public string Type { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public string Account { get; set; }
+
     }
-  }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+        }
+    }
 }
